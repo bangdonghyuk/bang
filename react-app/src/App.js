cb4cd9c0bbe928f45a5eddc27fc3f3327c5cd52c
bangdonghyuk <49140497+bangdonghyuk@users.noreply.github.com>
2021-01-07 17:47:14 +0900

    리엑트
    리엑트

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: "read",
-      selected_content_id: 2,
+      mode: "create",
+      selected_content_id: 1,
       subject: { title: "WEB", sub: "world Wid web!" },
       welcome: { title: "Welcome", desc: "Hello, react!!" },
       contents: [
@@ -23,25 +27,90 @@ class App extends Component {
       ],
     };
   }
-  render() {
-    console.log("App render");
+  getReadContent() {
+    var i = 0;
+    while (i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if (data.id === this.state.selected_content_id) {
+        return data;
+        break;
+      }
+      i = i + 1;
+    }
+  }
+  getContent() {
     var _title,
-      _desc = null;
+      _desc,
+      _article = null;
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === "read") {
-      var i = 0;
-      while (i < this.state.contents.length) {
-        var data = this.state.contents[i];
-        if (data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-        }
-        i = i + 1;
-      }
+      _content = this.this.getReadContent();
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === "create") {
+      _article = (
+        <CreateContent
+          onSubmit={function (_title, _desc) {
+            // add content to state.contents
+            this.max_content_id = this.max_content_id + 1;
+            // this.state.contents.push({
+            //   id: this.max_content_id,
+            //   title: _title,
+            //   desc: _desc,
+            // });
+            var _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            // var newContents = Array.from(this.state.contents);
+            // newContents.push({
+            //   id: this.max_content_id,
+            //   title: _title,
+            //   desc: _desc,
+            // });
+            this.setState({
+              contents: _contents,
+            });
+            console.log(_title, _desc);
+          }.bind(this)}
+        ></CreateContent>
+      );
+    } else if (this.state.mode === "update") {
+      _article = (
+        <UpdateContent
+          onSubmit={function (_title, _desc) {
+            // add content to state.contents
+            this.max_content_id = this.max_content_id + 1;
+            // this.state.contents.push({
+            //   id: this.max_content_id,
+            //   title: _title,
+            //   desc: _desc,
+            // });
+            var _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            // var newContents = Array.from(this.state.contents);
+            // newContents.push({
+            //   id: this.max_content_id,
+            //   title: _title,
+            //   desc: _desc,
+            // });
+            this.setState({
+              contents: _contents,
+            });
+            console.log(_title, _desc);
+          }.bind(this)}
+        ></UpdateContent>
+      );
     }
-    console.log("render", this);
+    return _article;
+  }
+  render() {
     return (
       <div className="App">
         <Subject
@@ -51,32 +120,18 @@ class App extends Component {
             this.setState({ mode: "welcome" });
           }.bind(this)}
         ></Subject>
-        {/* <header>
-          <h1>
-            <a
-              href="/"
-              onClick={function (e) {
-                console.log(e);
-                e.preventDefault();
-                // this.state.mode = "welcome";
-                this.setState({
-                  mode: "welcome",
-                });
-              }.bind(this)}
-            >
-              {this.state.subject.title}
-            </a>
-          </h1>
-          {this.state.subject.sub}
-        </header> */}
-
         <TOC
           onChangePage={function (id) {
             this.setState({ mode: "read", selected_content_id: Number(id) });
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control
+          onChangeMode={function (_mode) {
+            this.setState({ mode: _mode });
+          }.bind(this)}
+        ></Control>
+        {this.getContent()}
       </div>
     );
   }
